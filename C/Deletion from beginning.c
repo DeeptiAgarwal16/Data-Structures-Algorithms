08a57847314b5ff4091fddea93024b61919a1a90
DeeptiAgarwal16 <115862867+DeeptiAgarwal16@users.noreply.github.com>
2023-07-20 08:17:19 +0530

    Create Deletion from beginning.c

---
+++ C/Deletion from beginning.c
@@ -0,0 +1,48 @@
+struct node
+{
+  int data;
+  struct node * link;
+};
+int main()
+{
+  struct node *start, *p, *temp, *n, *d;
+  int i,dat;
+  start=(struct node *)malloc(sizeof(struct node));
+  start->data=10;
+  start->link=NULL;
+  p=(struct node *)malloc(sizeof(struct node));
+  p->data=20;
+  start->link=p;
+  for(i=0;i<3;i++)
+  {
+    n=(struct node *)malloc(sizeof(struct node));
+    printf("Enter data : ");
+    scanf("%d",&dat);
+    n->data=dat;
+    p->link=n;
+    n->link=NULL;
+    p=p->link;
+  }
+  temp=start;
+  while(temp!=NULL)
+  {
+    printf("%d ",temp->data);
+    temp=temp->link;
+  }
+  temp=start;
+  if(temp==start)
+  {
+    d=start;
+    temp=temp->link;
+    start->link=NULL;
+    start=temp;
+    free(d);
+  }
+  printf("\n");
+  temp=start;
+  while(temp!=NULL)
+  {
+    printf("%d ",temp->data);
+    temp=temp->link;
+  }
+}

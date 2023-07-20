94ab037576379e83d039293d061a6243e3bfa7d2
DeeptiAgarwal16 <115862867+DeeptiAgarwal16@users.noreply.github.com>
2023-07-20 08:32:33 +0530

    Create 10. Inserion at end in Circular Linked List.c

---
+++ C/10. Inserion at end in Circular Linked List.c
@@ -0,0 +1,52 @@
+struct node
+{
+  int data;
+  struct node * link;
+};
+int main()
+{
+  struct node *start, *p, *temp, *n;
+  int i,d;
+  start=(struct node *)malloc(sizeof(struct node));
+  start->data=10;
+  start->link=NULL;
+  p=(struct node *)malloc(sizeof(struct node));
+  p->data=20;
+  start->link=p;
+  for(i=0;i<3;i++)
+  {
+    n=(struct node *)malloc(sizeof(struct node));
+    printf("Enter data : ");
+    scanf("%d",&d);
+    n->data=d;
+    p->link=n;
+    n->link=start;
+    p=p->link;
+  }
+  temp=start;
+  while(1)
+  {
+    printf("%d ",temp->data);
+    if(temp->link==start)
+    break;
+    temp=temp->link;
+  }
+  n=(struct node *)malloc(sizeof(struct node));
+  while(p->link!=start)
+  {
+    temp=p;
+    p=p->link;
+  }
+  n->data=100;
+  n->link=start;
+  temp->link=n;
+  temp=start;
+  printf("\n");
+  while(1)
+  {
+    printf("%d ",temp->data);
+    if(temp->link==start)
+    break;
+    temp=temp->link;
+  }
+}

9f1a5265eb083c71fa7c0de8b97a03814b3c2a13
DeeptiAgarwal16 <115862867+DeeptiAgarwal16@users.noreply.github.com>
2023-07-20 08:28:36 +0530

    Create 7. Doubly linked list traversal.c

---
+++ C/7. Doubly linked list traversal.c
@@ -0,0 +1,32 @@
+struct node
+{
+  struct node *prev;
+  int data;
+  struct node *next;
+};
+int main()
+{
+  struct node *start, *n, *p;
+  int d=20,i;
+  n=(struct node *)malloc(sizeof(struct node));
+  n->data=10;
+  n->prev=NULL;
+  n->next=NULL;
+  start=n;
+  for(i=1;i<=4;i++)
+  {
+    p=(struct node *)malloc(sizeof(struct node));
+    p->data=d;
+    d+=10;
+    p->prev=n;
+    p->next=NULL;
+    n->next=p;
+    n=n->next;
+  }
+  p=start;
+  while(p!=NULL)
+  {
+    printf("%d ",p->data);
+    p=p->next;
+  }
+}

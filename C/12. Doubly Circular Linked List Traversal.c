f4d5cd63e162cd7c62ca5e7ad9fc9dd6a9594eb4
DeeptiAgarwal16 <115862867+DeeptiAgarwal16@users.noreply.github.com>
2023-07-20 08:40:27 +0530

    Create 12. Doubly Circular Linked List Traversal.c

---
+++ C/12. Doubly Circular Linked List Traversal.c
@@ -0,0 +1,35 @@
+struct node
+{
+  struct node *prev;
+  int data;
+  struct node *next;
+};
+int main()
+{
+  struct node *start, *p, *n;
+  int i,d=20;
+  p=(struct node *)malloc(sizeof(struct node));
+  start=p;
+  p->data=10;
+  p->prev=start;
+  p->next=start;
+  for(i=1;i<=4;i++)
+  {
+    n=(struct node *)malloc(sizeof(struct node));
+    n->data=d;
+    d+=10;
+    n->prev=p;
+    n->next=start;
+    p->next=n;
+    start->prev=n;
+    p=p->next;
+  }
+  p=start;
+  while(1)
+  {
+    printf("%d ",p->data);
+    if(p->next==start)
+    break;
+    p=p->next;
+  }
+}
